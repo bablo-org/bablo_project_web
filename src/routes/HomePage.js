34ebdb41623a9846e651d52d676bdba7c0ba1249
Anton Ovchinnikov <92427935+AntonOvc@users.noreply.github.com>
2023-03-17 20:44:38 +0700

    Update HomePage.js

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,10 +1,7 @@
-import { useContext } from "react";
 import Header from "../components/Header/Header";
-import { AuthContext } from "../context/Auth";
-import { auth, signOut } from "../services/firebase";
+
 
 const HomePage = () => {
-  const authContext = useContext(AuthContext)
   return (
     <Header/>
   );
