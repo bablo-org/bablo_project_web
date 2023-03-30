4bbe2d54dd63f7de34e34fa518b868890b2dbbc0
Pr0bka <VissZuKr3in@yandex.ru>
2023-03-30 22:22:44 +0700

    Feat: added sort logic to transactions

--- src/components/Transactions/TransactionsList.js
+++ src/components/Transactions/TransactionsList.js
@@ -1,5 +1,5 @@
 import useHomeApi from '../../hooks/useHomeApi';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import TransactionItem from './TransactionItem';
 import classes from './TransactionsList.module.css';
 import Spinner from '../Spinner/Spinner';
@@ -7,6 +7,11 @@ const TransactionsList = () => {
   const [transactions, setTransactions] = useState([]);
   const [users, setUsers] = useState([]);
   const { getTransactions, getUsers, loading } = useHomeApi();
+  const sortedTransactions = useMemo(() => {
+    return transactions.sort(
+      (obj1, obj2) => new Date(obj2.date) - new Date(obj1.date)
+    );
+  }, [transactions]);
   useEffect(() => {
     getTransactions().then(setTransactions);
     getUsers().then(setUsers);
@@ -23,25 +28,25 @@ const TransactionsList = () => {
     return idToName;
   };
 
+  const transformedTransactions = sortedTransactions.map((transaction) => (
+    <TransactionItem
+      key={transaction.id}
+      sender={formatUserName(transaction.sender)}
+      receiver={formatUserName(transaction.receiver)}
+      currency={transaction.currency}
+      amount={transaction.amount}
+      description={transaction.description}
+      date={transaction.date}
+      status={transaction.status}
+      created={transaction.created}
+      updated={transaction.updated}
+    />
+  ));
+
   return (
     <section className={classes.transactions}>
       <div className={classes.card}>
-        <ul>
-          {transactions.map((transaction) => (
-            <TransactionItem
-              key={transaction.id}
-              sender={formatUserName(transaction.sender)}
-              receiver={formatUserName(transaction.receiver)}
-              currency={transaction.currency}
-              amount={transaction.amount}
-              description={transaction.description}
-              date={transaction.date}
-              status={transaction.status}
-              created={transaction.created}
-              updated={transaction.updated}
-            />
-          ))}
-        </ul>
+        <ul>{transformedTransactions}</ul>
       </div>
     </section>
   );
