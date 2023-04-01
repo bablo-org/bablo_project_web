03d94551e3935df33c5f46cf31cc57e15f58e6b1
Pr0bka <VissZuKr3in@yandex.ru>
2023-04-01 19:04:22 +0700

    Update TransactionsList.js

--- src/components/Transactions/TransactionsList.js
+++ src/components/Transactions/TransactionsList.js
@@ -7,41 +7,42 @@ const TransactionsList = () => {
   const [transactions, setTransactions] = useState([]);
   const [users, setUsers] = useState([]);
   const { getTransactions, getUsers, loading } = useHomeApi();
-  const sortedTransactions = useMemo(() => {
-    return transactions.sort(
-      (obj1, obj2) => new Date(obj2.date) - new Date(obj1.date)
-    );
-  }, [transactions]);
+
   useEffect(() => {
     getTransactions().then(setTransactions);
     getUsers().then(setUsers);
   }, []);
-
-  if (loading) {
-    return <Spinner />;
-  } else if (!loading && transactions.length === 0) {
-    return <p className={classes.noTransactionsAvailable}>Суй ананас в жопу</p>;
-  }
 
   const formatUserName = (incomingId) => {
     const idToName = users.find((user) => user.id === incomingId)?.name;
     return idToName;
   };
 
-  const transformedTransactions = sortedTransactions.map((transaction) => (
-    <TransactionItem
-      key={transaction.id}
-      sender={formatUserName(transaction.sender)}
-      receiver={formatUserName(transaction.receiver)}
-      currency={transaction.currency}
-      amount={transaction.amount}
-      description={transaction.description}
-      date={transaction.date}
-      status={transaction.status}
-      created={transaction.created}
-      updated={transaction.updated}
-    />
-  ));
+  const transformedTransactions = useMemo(() => {
+    const sortedTransactions = transactions.sort(
+      (obj1, obj2) => new Date(obj2.date) - new Date(obj1.date)
+    );
+    return sortedTransactions.map((transaction) => (
+      <TransactionItem
+        key={transaction.id}
+        sender={formatUserName(transaction.sender)}
+        receiver={formatUserName(transaction.receiver)}
+        currency={transaction.currency}
+        amount={transaction.amount}
+        description={transaction.description}
+        date={transaction.date}
+        status={transaction.status}
+        created={transaction.created}
+        updated={transaction.updated}
+      />
+    ));
+  }, [transactions]);
+
+  if (loading || users === undefined) {
+    return <Spinner />;
+  } else if (!loading && transactions.length === 0) {
+    return <p className={classes.noTransactionsAvailable}>Суй ананас в жопу</p>;
+  }
 
   return (
     <section className={classes.transactions}>
