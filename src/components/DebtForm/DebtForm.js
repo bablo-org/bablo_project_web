cfc6c7dec4456c132a400bb9e4dc38f56fb8d61c
Iliustrator <120363767+Iliustrator@users.noreply.github.com>
2023-04-05 21:04:56 +0700

    feat: add multiTransactions on DebtForm

--- src/components/DebtForm/DebtForm.js
+++ src/components/DebtForm/DebtForm.js
@@ -6,11 +6,12 @@ import useHomeApi from "../../hooks/useHomeApi";
 const DebtForm = () => {
   const [users, setUsers] = useState([]);
   const [sender, setSender] = useState([]);
+  const [isSenderSelected, setIsSenderSelected] = useState(true);
   const [receiver, setReceiver] = useState([]);
+  const [isReceiverSelected, setIsReceiverSelected] = useState(true);
   const [enteredCurrency, setEnteredCurrency] = useState(``);
-  const [isFormValid, setIsFormIsValid] = useState(true);
-  const [isSumValid, setIsSumValid] = useState(true);
   const [enteredSum, setEnteredSum] = useState(``);
+  const [enteredUsersSum, setEnteredUsersSum] = useState({});
   const [enteredDescription, setEnteredDescription] = useState(``);
   const [enteredDate, setEnteredDate] = useState();
   const [currencies, setCurrencies] = useState([]);
@@ -22,11 +23,23 @@ const DebtForm = () => {
     getUsers().then(setUsers);
   }, []);
 
+  useEffect(() => {
+    sender.length > 0 && setIsSenderSelected(true);
+  }, [sender]);
+
+  useEffect(() => {
+    receiver.length > 0 && setIsReceiverSelected(true);
+  }, [receiver]);
+
   const currencyInputChangeHandler = (event) => {
     setEnteredCurrency(event.target.value);
   };
   const sumInputChangeHandler = (event) => {
     setEnteredSum(event.target.value);
+  };
+  const usersSumInputChangeHandler = (event, user) => {
+    const newUsersSum = { ...enteredUsersSum, [user.id]: event.target.value };
+    setEnteredUsersSum(newUsersSum);
   };
   const descriptionInputChangeHandler = (event) => {
     setEnteredDescription(event.target.value);
@@ -48,39 +61,38 @@ const DebtForm = () => {
 
   const submissionOfDebtHandler = (event) => {
     event.preventDefault();
-    const transformedSum = Array.from(JSON.stringify(enteredSum));
-    if (
-      sender === undefined ||
-      receiver === undefined ||
-      enteredCurrency === `` ||
-      enteredSum === `` ||
-      enteredDescription === `` ||
-      enteredDate === ``
-    ) {
-      setIsFormIsValid(false);
-    } else if (
-      transformedSum.indexOf(`-`) === 1 ||
-      transformedSum.indexOf(`.`) === 1 ||
-      transformedSum.includes(`-`) ||
-      transformedSum.includes(`+`) ||
-      transformedSum.includes(`e`) ||
-      transformedSum.includes(`=`)
-    ) {
-      setIsSumValid(false);
+    if (sender.length === 0) {
+      setIsSenderSelected(false);
+    } else if (receiver.length === 0) {
+      setIsReceiverSelected(false);
     } else {
-      const debtData = {
-        sender: sender,
-        receiver: receiver,
-        currency: enteredCurrency,
-        amount: parseInt(enteredSum),
-        description: enteredDescription,
-        date: enteredDate ? new Date(enteredDate).toISOString() : undefined,
-      };
-      setIsFormIsValid(true);
-      postTransactions(debtData);
-      setIsSumValid(true);
+      setIsSenderSelected(true);
+      setIsReceiverSelected(true);
+
+      sender.forEach((id) => {
+        const debtData = {
+          sender: id,
+          receiver: receiver[0],
+          currency: enteredCurrency,
+          amount: parseFloat(
+            sender.length === 1 ? enteredSum : enteredUsersSum[id]
+          ),
+          description: enteredDescription,
+          date: enteredDate ? new Date(enteredDate).toISOString() : undefined,
+        };
+        postTransactions(debtData);
+      });
       clearForm();
     }
+  };
+
+  const shareSum = () => {
+    const sharedSum = Math.round((enteredSum / sender.length) * 100) / 100;
+    const newUsersSum = {};
+    sender.forEach((id) => {
+      newUsersSum[id] = sharedSum;
+    });
+    setEnteredUsersSum(newUsersSum);
   };
 
   const currencySelector = (
@@ -88,6 +100,7 @@ const DebtForm = () => {
       className={classes.currencySelector}
       value={enteredCurrency}
       onChange={currencyInputChangeHandler}
+      required
     >
       <option value="" defaultValue disabled hidden>
         Choose Value
@@ -103,7 +116,7 @@ const DebtForm = () => {
   return (
     <Fragment>
       <div>
-      <label className={classes.addDebtlabel}>Должник</label>
+        <label className={classes.addDebtlabel}>Должник</label>
         <AvatarsList
           users={users}
           loading={loading}
@@ -111,19 +124,21 @@ const DebtForm = () => {
           onUserSelected={setSender}
           blockedUserIds={undefined}
         />
-        
+        {!isSenderSelected && <p>Выберите Должника</p>}
+
         {sender.length > 0 && (
           <>
-          <label className={classes.addDebtlabel}>Получатель</label>
-          <AvatarsList
-            users={users}
-            loading={loading}
-            error={error}
-            onUserSelected={setReceiver}
-            blockedUserIds={sender}
-          />
+            <label className={classes.addDebtlabel}>Получатель</label>
+            <AvatarsList
+              users={users}
+              loading={loading}
+              error={error}
+              onUserSelected={setReceiver}
+              blockedUserIds={sender}
+            />
           </>
         )}
+        {!isReceiverSelected && <p>Выберите Получателя</p>}
       </div>
       <div className={classes.container}>
         <form className={classes.form} onSubmit={submissionOfDebtHandler}>
@@ -135,12 +150,41 @@ const DebtForm = () => {
             <label htmlFor="sum">Сумма</label>
             <input
               value={enteredSum}
-              type="number"
+              type="text"
               id="sum"
               onChange={sumInputChangeHandler}
-              pattern="[0-9]"
+              pattern="(?:0|[1-9]\d*)(?:\.\d{1,2})?"
+              title="Введеная сумма должна быть в формате xxx.xx и не начинаться с 0 если это не дробь вида 0.хх"
+              required
             />
           </div>
+          {sender.length > 1 && (
+            <div className={classes.actions}>
+              <button type="button" onClick={shareSum} className={classes.sum}>
+                Поделить поровну
+              </button>
+            </div>
+          )}
+          {sender.length > 1 &&
+            sender.map((id) => {
+              const user = users.find((item) => item.id === id);
+              return (
+                <div className={classes.control} key={`sum ${user.id}`}>
+                  <label htmlFor="sum">Сумма {user.name}</label>
+                  <input
+                    value={enteredUsersSum[user.id] ?? ""}
+                    type="text"
+                    id={`sum ${user.id}`}
+                    onChange={(event) =>
+                      usersSumInputChangeHandler(event, user)
+                    }
+                    pattern="(?:0|[1-9]\d*)(?:\.\d{1,2})?"
+                    title="Введеная сумма должна быть в формате xxx.xx и не начинаться с 0 если это не дробь вида 0.хх"
+                    required
+                  />
+                </div>
+              );
+            })}
           <div className={classes.control}>
             <label htmlFor="description">Описание</label>
             <input
@@ -148,6 +192,7 @@ const DebtForm = () => {
               type="text"
               id="description"
               onChange={descriptionInputChangeHandler}
+              required
             />
           </div>
           <div className={classes.control}>
@@ -157,6 +202,7 @@ const DebtForm = () => {
               type="date"
               id="date"
               onChange={dateInputChangeHandler}
+              required
             />
           </div>
           <div className={classes.actions}>
@@ -165,10 +211,6 @@ const DebtForm = () => {
             </button>
             <button className={classes.submit}>Confirm</button>
           </div>
-          {!isFormValid ? (
-            <p>Все поля должны быть заполнены... Лох</p>
-          ) : undefined}
-          {!isSumValid ? <p>В поле сумма должно быть число</p> : undefined}
         </form>
       </div>
     </Fragment>
