b7b0907818f7ec8a6a9c5e3b27e39688f65314ce
Iliustrator <120363767+Iliustrator@users.noreply.github.com>
2023-04-07 18:09:56 +0700

    feat: materialUI implemented at AuthorizedLayout

--- src/layouts/AuthorizedLayout.js
+++ src/layouts/AuthorizedLayout.js
@@ -1,14 +1,188 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import HistoryIcon from "@mui/icons-material/History";
+import SummarizeIcon from "@mui/icons-material/Summarize";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header/Header";
+import { auth, signOut } from "../services/firebase";
+import { useContext } from "react";
+import { AuthContext } from "../context/Auth";
+import { useNavigate } from "react-router-dom";
+import { PATHES } from "../routes";
+import { useMemo } from "react";
 
+const drawerWidth = 240;
 
-const AuthorizedLayout = () => {
-  return (
-    <>
-      <Header/>
-      <Outlet/>
-    </>
+function AuthorizedLayout(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const listItemButtons = useMemo(() => ([
+    {
+      name: "Добавить транзакцию",
+      path: PATHES.ADD_TRANSACTION,
+      icon: <ReceiptIcon />,
+    },
+    {
+      name: "История",
+      path: PATHES.HISTORY,
+      icon: <HistoryIcon />,
+    },
+    {
+      name: "Итоги",
+      path: PATHES.SUMMARY,
+      icon: <SummarizeIcon />,
+    },
+    {
+      name: "Профиль",
+      path: PATHES.PROFILE,
+      icon: <AccountCircleIcon />,
+    },
+  ]), []);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {listItemButtons.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton onClick={() => navigate(item.path)}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        <ListItem key={"Выйти"} disablePadding>
+          <ListItemButton
+            onClick={() =>
+              signOut(auth).then(() => {
+                authContext.setUser(null);
+              })
+            }
+          >
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Выйти"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </div>
   );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Bablo project
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+}
+
+AuthorizedLayout.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
 export default AuthorizedLayout;
