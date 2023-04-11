6661e8b7ffcd0022e0211d6b6df8b2b79c99152d
Anton Ovchinnikov <92427935+AntonOvc@users.noreply.github.com>
2023-04-11 15:35:42 +0700

    feat: add content type header to default query fn

--- src/queries/index.js
+++ src/queries/index.js
@@ -4,7 +4,11 @@ import { auth } from "../services/firebase";
 export const defaultQueryFn = async ({ queryKey, requestOptions = {} }) => {
   const _apiBase = "https://api-5cpqvpy3cq-uc.a.run.app";
   const _token = auth.currentUser?.accessToken;
-  const updatedHeaders = { ...requestOptions?.headers, Authorization: _token };
+  const contentType =
+    requestOptions?.method && requestOptions.method.toUpperCase() !== "GET"
+      ? { "Content-Type": "application/json" }
+      : {};
+  const updatedHeaders = { ...requestOptions?.headers, Authorization: _token, ...contentType };
   requestOptions.headers = updatedHeaders;
   const response = await fetch(`${_apiBase}/${queryKey[0]}`, requestOptions);
   if (!response.ok) {
