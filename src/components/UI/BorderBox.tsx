020f589099a33d29050293acfa3d63af4f16c9fc
Pr0bka <VissZuKr3in@yandex.ru>
2023-06-06 19:07:56 +0800

    Fix: review comments

--- src/components/UI/BorderBox.tsx
+++ src/components/UI/BorderBox.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 
-function BorderBox({ children }: { children: JSX.Element }) {
+type Props = { children: JSX.Element };
+function BorderBox({ children }: Props) {
   return (
     <Box sx={{ border: 1, borderRadius: 8, marginBottom: 2 }}>{children}</Box>
   );
