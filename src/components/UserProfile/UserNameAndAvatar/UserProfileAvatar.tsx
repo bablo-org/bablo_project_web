9843a1a503b53558558d1dfa9cd5e260093a478a
Pr0bka <VissZuKr3in@yandex.ru>
2023-06-10 21:53:15 +0800

    Fix: review comments

--- src/components/UserProfile/UserNameAndAvatar/UserProfileAvatar.tsx
+++ src/components/UserProfile/UserNameAndAvatar/UserProfileAvatar.tsx
@@ -32,7 +32,7 @@ function UserProfileAvatar({ currentUser, deleteAvatar }: Props) {
     >
       <UserAvatar
         xs={50}
-        sm={50}
+        sm={70}
         md={100}
         key={currentUser.id}
         name={currentUser.name}
