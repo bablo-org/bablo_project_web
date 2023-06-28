2920be1a72fce439a5c8a4da7f570c46ecbf620e
Pr0bka <VissZuKr3in@yandex.ru>
2023-06-28 21:51:47 +0700

    Feat: implemented i18n

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,17 +1,44 @@
-import { Box, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Box, Button, Typography } from '@mui/material';
+import { useMemo, useState } from 'react';
+import i18n from '../../i18n';
 import BorderBox from '../UI/BorderBox';
 
 function HomePage() {
+  const { t } = useTranslation();
+  const [isEnglish, setIsEnglish] = useState(true);
+  const lng = useMemo(() => {
+    return i18n.language;
+  }, [i18n.language]);
+
+  const handleClick = () => {
+    if (!isEnglish) {
+      setIsEnglish(true);
+      i18n.changeLanguage('en-US');
+    }
+    if (isEnglish) {
+      setIsEnglish(false);
+      i18n.changeLanguage('ru');
+    }
+  };
   return (
-    <BorderBox marginProp={2}>
-      <>
-        <Typography>Большегриб</Typography>
-        <Box
-          component='img'
-          src='https://wow.zamimg.com/modelviewer/live/webthumbs/npc/166/83622.webp'
-        />
-      </>
-    </BorderBox>
+    <>
+      <BorderBox marginProp={2}>
+        <>
+          <Typography>{t('homePage.sexyMushroom')}</Typography>
+          <Box
+            component='img'
+            src='https://wow.zamimg.com/modelviewer/live/webthumbs/npc/166/83622.webp'
+          />
+        </>
+      </BorderBox>
+      <Button onClick={handleClick} variant='contained'>
+        <Box display='flex' flexDirection='column'>
+          {t('homePage.languageChangeButton')}
+          <Typography align='center'>{lng}</Typography>
+        </Box>
+      </Button>
+    </>
   );
 }
 
